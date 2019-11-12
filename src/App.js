7e19776225fe3fa38c3ec143b3f4c18687d07112
ggsuccess <ggsuccess@naver.com>
2019-11-12 17:41:32 +0900

    categories

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import NewsList from './components/NewsList';
+import Categories from './components/Categories';
 
 const App = () => {
-  return <NewsList />;
+  return (
+    <React.Fragment>
+      <Categories />
+      <NewsList />
+    </React.Fragment>
+  );
 };
 
 export default App;
