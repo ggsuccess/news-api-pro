1e2060298b80a99e576780ce44e21df4f69f35c4
ggsuccess <ggsuccess@naver.com>
2019-11-13 19:09:18 +0900

    appRouting

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import React, { useState, useCallback } from 'react';
-import NewsList from './components/NewsList';
-import Categories from './components/Categories';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import NewsPage from '../src/components/NewsPage';
 
 const App = () => {
-  const [category, setCategory] = useState('all');
-  const onSelect = useCallback(category => setCategory(category), []);
-  return (
-    <React.Fragment>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />
-    </React.Fragment>
-  );
+  return <Route path="/:category?" component={NewsPage} />;
 };
 
 export default App;
